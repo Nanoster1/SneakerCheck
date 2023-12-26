9718645c54c26bbc397257e315f0df2d61929409
Nanoster1 <danil2002.11@mail.ru>
2023-12-26 22:27:09 +0500

    Added server spa hosting

--- src/WebApi/SneakerCheck.WebApi/Program.cs
+++ src/WebApi/SneakerCheck.WebApi/Program.cs
@@ -45,6 +45,10 @@ var app = builder.Build();
     app.UseAuthorization();
 
     app.MapControllers();
+
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+    app.MapFallbackToFile("index.html");
 }
 
 app.MigrateDatabase();
